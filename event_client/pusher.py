cce2faec1e56a40ec10dc85f04dd38a8cf3cb8f0
Jan Karásek <devel@hkar.eu>
2016-06-06 13:54:11 +0200

    added continue on error

--- event_client/pusher.py
+++ event_client/pusher.py
@@ -5,5 +5,8 @@ class Pusher:
     @staticmethod
     def push_all_new(api_client: EventClient, event_logger: EventsLogger):
         for event in event_logger.read_new():
-            api_client.push_event(event.event_type, event.payload)
-            event.set_ack()
+            try:
+                api_client.push_event(event.event_type, event.payload)
+                event.set_ack()
+            except:
+                pass
