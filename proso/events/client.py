db7b1ddf3e5fa2678bb8cc116a2899e6e7461963
Jan Papousek <jan.papousek@gmail.com>
2016-08-17 10:05:19 +0200

    refactor naming

--- proso/events/client.py
+++ proso/events/client.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import simplejson as json
 import requests
 import hashlib
 import psycopg2
 import datetime
+import datetime
+import zlib
 
 
 class EventClient:
@@ -54,3 +54,57 @@ class EventClient:
             raise Exception("Event storage error. Status: %s" % req.status_code)
 
         return psycopg2.connect(**req.json())
+
+
+class EventsLogger:
+    def __init__(self, log_path: str, source: str):
+        self.event_file = EventFile(log_path)
+        self.source = source
+
+    def emit(self, event_type: str, data: dict, tags: list = [], time: datetime.datetime = datetime.datetime.now()):
+        """
+        Add new event to datastore (file).
+        """
+
+        data['_type'] = event_type
+        data['tags'] = tags
+        data['datetime'] = str(time)
+        data['source'] = str(self.source)
+
+        self.event_file.write(data)
+
+
+class EventFile:
+    def __init__(self, log_path: str):
+        self.log_path = log_path
+
+    def write(self, data: dict):
+        """
+        Write dict to file (JSON + CRC)
+        """
+        data = json.dumps(data)
+        with open(self.log_path, 'a') as f:
+            f.write("%s %s\n" % (data, zlib.crc32(data.encode())))
+            f.flush()
+
+    def read_events(self):
+        """
+        Read events from file and validate CRC.
+        Damaged records are skipped.
+        """
+        with open(self.log_path, 'r') as f:
+            for line in f:
+                last_space = line.rfind(' ')
+                if int(line[last_space + 1:-1]) == zlib.crc32(line[:last_space].encode()):
+                    yield json.loads(line[:last_space])
+                else:
+                    pass  # skip damaged records
+
+
+class Pusher:
+    @staticmethod
+    def push_all(api_client: EventClient, event_file: EventFile):
+        for event in event_file.read_events():
+            event_type = event['_type']
+            del event['_type']
+            api_client.push_event(event_type, event)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
     LICENSE = f.read()
 
 setup(
-    name='event_client',
+    name='proso-events-client',
     version='0.0.1',
     description='Client for "event storage"."',
     long_description=README,
@@ -21,6 +21,7 @@ setup(
     license=LICENSE,
     include_package_data = True,
     packages=find_packages(exclude=('tests', 'docs')),
+    namespace_packages=['proso', 'proso.events'],
     install_requires=[
         str(r.req)
         for r in parse_requirements(DIR + '/requirements.txt', session=False)
